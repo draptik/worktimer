2922e8c4440f6464ea03021041089b153be2d46e
Patrick Drechsler/Win7 <patrick@pdrechsler.de>
2011-06-14 21:26:49 +0200

    added hour lines

--- WorkTimer/WorkTimer/WorkProgressUserControl.xaml.cs
+++ WorkTimer/WorkTimer/WorkProgressUserControl.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace WorkTimer
 {
@@ -16,29 +18,86 @@ namespace WorkTimer
 
         public void Init()
         {
-            var zeroPosX = rctMain.Margin.Left;
+            InitHourLines();
 
-            // Target time
-            const double targetTime = 8.0;
-            var targetPos = GetPos(targetTime);
-
-            rctTargetTime.Margin = SetElementPos(zeroPosX + targetPos, rctTargetTime);
-            rctTargetTime.Visibility = Visibility.Visible;
-
-            // Min times
+            const double targetTime = 8.75;
+            InitTargetLine(targetTime);
+            
             const double minTimeStart = 6.0;
-            rctMinTime.Margin = SetElementPos(zeroPosX + GetPos(minTimeStart), rctMinTime);
+            InitMinTimes(minTimeStart);
+        }
+
+        private void InitMinTimes(double minTimeStart)
+        {
+            rctMinTime.Margin = SetElementPos(GetPos(minTimeStart), rctMinTime);
             rctMinTime.Width = GetPos(0.75);
             rctMinTime.Visibility = Visibility.Visible;
         }
 
+        private void InitHourLines()
+        {
+            const int numHours = 10;
+            for (var i = 1; i <= numHours; i++)
+            {
+                var xPos = GetPos(i);
+                var line = AddLine(xPos);
+                var label = AddLabel(i, xPos);
+                gridMain.Children.Add(line);
+                gridMain.Children.Add(label);
+            }
+        }
+
+        private Label AddLabel(int i, double xPos)
+        {
+            const double offsetX = 8;
+            const double offsetY = 17;
+            return new Label
+                   {
+                       Margin = new Thickness(xPos - offsetX, GetHeightTop()-offsetY, 0, 0),
+                       Content = i.ToString(),
+                       FontSize = 8,
+                       Height = offsetY+5,
+                       VerticalAlignment = VerticalAlignment.Top,
+                   };
+        }
+
+        private Line AddLine(double xPos)
+        {
+            return new Line
+                   {
+                       Stroke = Brushes.LightSteelBlue,
+                       X1 = xPos,
+                       X2 = xPos,
+                       Y1 = GetHeightTop(),
+                       Y2 = GetHeightBottom(),
+                       StrokeThickness = 1,
+                       StrokeDashArray = new DoubleCollection {2, 2},
+                       Opacity = 50
+                   };
+        }
+
+        private void InitTargetLine(double targetTime)
+        {
+            var targetPos = GetPos(targetTime);
+            var line = new Line
+            {
+                Stroke = Brushes.Green,
+                X1 = targetPos,
+                X2 = targetPos,
+                Y1 = GetHeightTop(),
+                Y2 = GetHeightBottom(),
+                StrokeThickness = 2
+            };
+            gridMain.Children.Add(line);
+        }
+        
 
         public void UpdateCurrentPos(TimeSpan timeSpent)
         {
-            var currentPos = GetPos(timeSpent.TotalMinutes / 60.0);
+            var currentPos = rctMain.Margin.Left + GetPos(timeSpent.TotalMinutes / 60.0);
             rctCurrent.Width = currentPos;
         }
-        
+
         private double GetPos(double d)
         {
             var maxPos = rctMain.Width;
@@ -49,6 +108,15 @@ namespace WorkTimer
         private static Thickness SetElementPos(double pos, FrameworkElement element)
         {
             return new Thickness(pos, element.Margin.Top, element.Margin.Right, element.Margin.Bottom);
+        }
+
+        private double GetHeightBottom()
+        {
+            return rctMain.Margin.Top + rctMain.Height;
+        }
+        private double GetHeightTop()
+        {
+            return rctMain.Margin.Top;
         }
     }
 }
